6ca0d6987062376ddebcc8d84fc985a8f5532e16
RutvikCodez <rutvikdarji21@gnu.ac.in>
2025-08-14 10:10:49 +0530

    create db.js file for connecting with neon database

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,34 @@
 import { config } from "dotenv";
 import express from "express";
+import { sql } from "./config/db.js";
 
 config();
 const app = express();
 const PORT = process.env.PORT;
 
+async function initDB() {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transactions(
+      id SERIAL PRIMARY KEY,
+      user_id VARCHAR(255) NOT NULL,
+      title VARCHAR(255) NOT NULL,
+      amount DECIMAL(10,2) NOT NULL,
+      category VARCHAR(255) NOT NULL,
+      created_at DATE NOT NULL DEFAULT CURRENT_DATE
+    )`;
+    console.log("Database Intialized successfully");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
 app.get("/", (req, res) => {
   res.send("Working!");
 });
-app.listen(PORT, () => {
-  console.log("Server is up and running on PORT:", PORT);
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is up and running on PORT:", PORT);
+  });
 });
