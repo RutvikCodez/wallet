ff2eaacebd77f4be6dad72e92c22f261f50494d8
RutvikCodez <rutvikdarji21@gnu.ac.in>
2025-08-25 17:33:39 +0530

    feat(auth): integrate Clerk for authentication with sign-in and sign-up flows
    - Added `@clerk/clerk-expo` dependency for authentication.
    - Implemented `AuthRoutesLayout` to handle redirection based on authentication state.
    - Created `sign-in` and `sign-up` screens with email/password authentication and email verification.
    - Updated root layout to redirect unauthenticated users to the sign-in page.
    - Added user greeting and sign-out functionality in the main page.
    - Introduced `SafeScreen` component for consistent safe area handling.
    - Created `SignOutButton` component for user sign-out functionality.

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,14 @@
+import SafeScreen from "@/components/SafeScreen";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeScreen>
+        <Stack screenOptions={{ headerShown: false }} />
+      </SafeScreen>
+    </ClerkProvider>
+  );
 }
